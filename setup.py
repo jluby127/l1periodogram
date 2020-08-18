d663dff27e697df49daae6672b49ac17f90bd4e1
jluby127 <50976985+jluby127@users.noreply.github.com>
2020-08-18 18:21:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='L1_per',
-   version='0.1.0',
+   version='0.2.0',
    author='Jack Lubin',
    author_email='jlubin@uci.edu',
    license='LICENSE.txt',
